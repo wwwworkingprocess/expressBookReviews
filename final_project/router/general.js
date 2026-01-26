0462d4cd1ab540215d02ba78a9358eaa20713ef6
Gergely Marton <wwworkingprocess@gmail.com>
2026-01-26 15:56:30 +0100

    updated general.js using async endpoints

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -2,95 +2,117 @@ const express = require('express');
 const axios = require('axios');
 
 let books = require("./booksdb.js");
-let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 
 const public_users = express.Router();
 
-const getBooksSync = () => books;   // sync access to books, used by the 'internal' route
-public_users.get('/internal/books-sync', (req, res) => res.status(200).json(getBooksSync())); // service url for axios
-
-//
-const getBooksAsync = async () => {
-    try {
-      return await axios.get("http://localhost:5000/internal/books-sync").then( r => r.data).then(booksToArray)
-    } catch (error) {
-      console.error(error);
-      throw error; // bubbling up error to be handled by the request handlers .catch()
-    }
-}
-
-const booksToArray = (obj) => {
-    const wrapISBN = ([isbn, book]) => ({ ...book, ISBN: String(isbn) });
-
-    return Object.entries(obj).map(wrapISBN);
-}
+const booksToArray = (obj) => Object.entries(obj).map(([isbn, book]) => ({ ...book, ISBN: String(isbn) }));
 
 // Get the book list available in the shop
-public_users.get('/', (req, res) => getBooksAsync()
-    .then(booksToArray)
-    .then( (arr) => res.status(200).json(arr))
-    .catch((err) => res.status(500).json({ message: "Error during processing your request" })));
-    
+public_users.get('/', async (req, res) => {
+    const response = await axios.get(`http://localhost:5000/async/`); // using axios
+
+    res.status(200).json(response.data);
+});
+// called by axios: (the book list available in the shop)
+public_users.get('/async/', (req, res) => {
+    try {
+        res.status(200).json(booksToArray(books))
+    } catch (err) {
+        return res.status(500).json({ message: "Error during processing your request" });
+    }
+});  // called by axios (array of books)
+
+
+// Get all books based on title
+public_users.get('/title/:title', async function (req, res) {
+    const title = req.params.title;
+
+    axios.get(`http://localhost:5000/async/title/${encodeURIComponent(title)}`)
+        .then(response => res.status(200).json(response.data))
+        .catch(err => res.status(404).json({ message: `Title not found (${title})` }));
+});
+// called by axios: (all books based on title)
+public_users.get('/async/title/:title', (req, res) => {
+    try {
+        const title = req.params.title;
+
+        const booksArray = booksToArray(books);
+        const booksByTitle = booksArray.filter(b => b.title === title);
+
+        if (booksByTitle.length > 0) res.status(200).json(booksByTitle);
+        else res.status(404).json({ message: `Title not found (${title})` });
+    } catch (err) {
+        return res.status(500).json({ message: "Error during processing your request" });
+    }
+});
+
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn', async function (req, res) {
-    return getBooksAsync()
-        .then(booksToArray)
-        .then( (arr) => arr.find( b => b.ISBN === req.params.isbn) )
-        .then( (book) => (book) ? 
-            res.status(200).json(book) :
-            res.status(404).json({ message: "Unable to find book by ISBN" })
-        ).catch( (err) => res.status(500).json({ message: "Error during processing your request" }))
+    const isbn = req.params.isbn;
+
+    axios.get(`http://localhost:5000/async/isbn/${isbn}`)
+        .then(response => res.status(200).json(response.data))
+        .catch(err => res.status(404).json({ message: `ISBN not found (${isbn})` }));
 });
+// called by axios: (Get book details based on ISBN)
+public_users.get('/async/isbn/:isbn', (req, res) => {
+    try {
+        const isbn = req.params.isbn;
+        const bookByIsbn = books[isbn];
+
+        if (bookByIsbn) res.status(200).json(bookByIsbn);
+        else res.status(404).json({ message: `Book not found (${isbn})` });
+    } catch (err) {
+        return res.status(500).json({ message: "Error during processing your request" });
+    }
+});
+
 
 // Get book details based on author
-public_users.get('/author/:author',async function (req, res) {
-    return getBooksAsync()
-        .then(booksToArray)
-        .then( (arr) => arr.filter( b => b.author === req.params.author) )
-        .then( (booksMatchingAuthor) => (booksMatchingAuthor.length > 0) ? 
-            res.status(200).json(booksMatchingAuthor) :
-            res.status(404).json({ message:  "Unable to find book(s) by author" })
-        ).catch( (err) => res.status(500).json({ message: "Error during processing your request" }))
+public_users.get('/author/:author', async function (req, res) {
+    const author = req.params.author;
+
+    axios.get(`http://localhost:5000/async/author/${encodeURIComponent(author)}`)
+        .then(response => res.status(200).json(response.data))
+        .catch(err => res.status(404).json({ message: `Author not found (${author})` }));
+});
+// called by axios: (book details based on author)
+public_users.get('/async/author/:author', (req, res) => {
+    try {
+        const author = req.params.author;
+
+        const booksArray = booksToArray(books);
+        const booksByAuthor = booksArray.filter(b => b.author === author);
+
+        if (booksByAuthor.length > 0) res.status(200).json(booksByAuthor);
+        else res.status(404).json({ message: `Author not found (${title})` });
+    } catch (err) {
+        return res.status(500).json({ message: "Error during processing your request" });
+    }
 });
 
-// Get all books based on title
-public_users.get('/title/:title',async function (req, res) {
-    return getBooksAsync()
-        .then(booksToArray)
-        .then( (arr) => arr.filter( b => b.title === req.params.title) )
-        .then( (booksMatchingTitle) => (booksMatchingTitle.length > 0) ? 
-            res.status(200).json(booksMatchingTitle) :
-            res.status(404).json({ message:  "Unable to find book(s) by title" })
-        ).catch( (err) => res.status(500).json({ message: "Error during processing your request" }))
-});
 
-
-const doesExist = (username) => users.find( u => u.username === username) !== undefined;
-
-const authenticatedUser = (username,password)=>{ //returns boolean
-    let validusers = users.filter((user) => {
-        return user.username === username && user.password === password;
-      });
-      return validusers.length > 0;
-}
-
+const doesExist = (username) => users.find(u => u.username === username) !== undefined;
 
 // registered first
-public_users.post("/register", (req,res) => {
-    const username = req.body.username;
-    const password = req.body.password;
+public_users.post("/register", (req, res) => {
+    try {
+        const username = req.body.username;
+        const password = req.body.password;
 
-    if (!username) return res.status(404).json({ message: "Please provide a username!" });
-    if (!password) return res.status(404).json({ message: "Please provide a password!" });
-    if (doesExist(username)) return res.status(404).json({ message: `The username (${username}) already exists!` });
+        if (!username) return res.status(404).json({ message: "Please provide a username!" });
+        if (!password) return res.status(404).json({ message: "Please provide a password!" });
+        if (doesExist(username)) return res.status(404).json({ message: `The username (${username}) already exists!` });
 
-    users.push({ "username": username, "password": password });
+        users.push({ "username": username, "password": password });
 
-    return res.status(200).json({ message: `User (${username}) successfully registered. Now you can login` });
+        return res.status(200).json({ message: `User (${username}) successfully registered. Now you can login` });
+    } catch (err) {
+        return res.status(500).json({ message: "Error during processing your request" });
+    }
 });
-
 
 const findReviewsByISBN = async (isbn) => {
     const book = books[isbn];
@@ -99,18 +121,15 @@ const findReviewsByISBN = async (isbn) => {
 };
 
 //  Get book review
-public_users.get('/review/:isbn',async function (req, res) {
+public_users.get('/review/:isbn', async function (req, res) {
     try {
-      const isbn = req.params.isbn;
-      const reviews = await findReviewsByISBN(isbn);
-  
-      if (reviews !== null) {
-        return res.status(200).json(reviews);
-      } else {
-        return res.status(404).json({ message: "Unable to find book by ISBN" });
-      }
+        const isbn = req.params.isbn;
+        const reviews = await findReviewsByISBN(isbn);
+
+        if (reviews !== null) return res.status(200).json(reviews);
+        else return res.status(404).json({ message: "Unable to find book by ISBN" });
     } catch (err) {
-      return res.status(500).json({ message: "Error during processing your request" });
+        return res.status(500).json({ message: "Error during processing your request" });
     }
 });
 
